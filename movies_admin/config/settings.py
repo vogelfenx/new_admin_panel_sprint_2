5a7bf480bfdda3d295a2f6a7510660d59a615796
Kirill Fedyakov <kirill.fed.pro@gmail.com>
2023-01-26 00:20:34 +0100

    Clean django app configuration file

--- movies_admin/config/settings.py
+++ movies_admin/config/settings.py
@@ -87,8 +87,7 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
         'PORT': os.environ.get('DB_PORT', 5432),
         'OPTIONS': {
-            # Нужно явно указать схемы, с которыми будет работать приложение.
-           'options': '-c search_path=public,content',
+            'options': '-c search_path=public,content',
         },
     },
 }
