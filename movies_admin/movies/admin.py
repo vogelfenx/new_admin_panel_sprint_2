9cce2792a5cec897f76ebeaa6d56ea1de041112f
Kirill Fedyakov <kirill.fed.pro@gmail.com>
2023-01-26 17:39:53 +0100

    Add new model: Filmwork

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from movies.models import Genre
+from movies.models import Genre, Filmwork
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
     pass
--- movies_admin/movies/models.py
+++ movies_admin/movies/models.py
@@ -17,3 +17,27 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Filmwork(models.Model):
+
+    class FilmworkTypes(models.TextChoices):
+        MOVIE = 'F', 'Фильм'
+        TV_SHOW = 'S', 'ТВ-Шоу'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField('Название', max_length=255)
+    description = models.TextField('Описание', blank=True)
+    creation_date = models.DateField('Дата выхода', blank=True)
+    rating = models.FloatField('Рейтинг', blank=True)
+    type = models.CharField('Тип', max_length=1, choices=FilmworkTypes.choices)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'content"."film_work'
+        verbose_name = 'Кинопроизведение'
+        verbose_name_plural = 'Кинопроизведения'
+
+    def __str__(self):
+        return self.title
