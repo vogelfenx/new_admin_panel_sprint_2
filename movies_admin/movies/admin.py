58fd7b47a4be74dade0901d93a60ad5e44db80ec
Kirill Fedyakov <kirill.fed.pro@gmail.com>
2023-01-28 22:09:42 +0100

    Refactore admin.py module

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -3,16 +3,6 @@ from django.contrib import admin
 from movies.models import Filmwork, Genre, Person, GenreFilmwork, PersonFilmwork
 
 
-@admin.register(Genre)
-class GenreAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Person)
-class PersonAdmin(admin.ModelAdmin):
-    pass
-
-
 class GenreFilmWorkInline(admin.TabularInline):
     model = GenreFilmwork
     verbose_name = 'Жанр'
@@ -23,6 +13,16 @@ class PersonFilmworkInline(admin.TabularInline):
     model = PersonFilmwork
     verbose_name = 'Персона'
     verbose_name_plural = 'Персоны фильма'
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Filmwork)
