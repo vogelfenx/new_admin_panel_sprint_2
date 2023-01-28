abb29bf64c81cc5402a97ee93c6823380f8eda35
Kirill Fedyakov <kirill.fed.pro@gmail.com>
2023-01-29 00:47:24 +0100

    Add enhancements for Person list admin panel
    - enhance person list display
    - add person Search by specific fields

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import mark_safe
 
-from movies.models import Filmwork, Genre, Person, GenreFilmwork, PersonFilmwork
+from movies.models import (Filmwork, Genre, GenreFilmwork, Person,
+                           PersonFilmwork)
 
 
 class GenreFilmWorkInline(admin.TabularInline):
@@ -13,12 +16,6 @@ class PersonFilmworkInline(admin.TabularInline):
     model = PersonFilmwork
     verbose_name = 'Персона'
     verbose_name_plural = 'Персоны фильма'
-
-
-
-@admin.register(Person)
-class PersonAdmin(admin.ModelAdmin):
-    pass
 
 
 @admin.register(Filmwork)
@@ -36,3 +33,29 @@ class GenreAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     search_fields = ('name', 'description')
 
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'get_filmworks')
+    search_fields = ('full_name', 'personfilmwork__film_work__title')
+
+    @admin.display(ordering='filmwork__title', description='Кинопроизведения')
+    def get_filmworks(self, person):
+        """Return filmworks of the given person.
+
+        Args:
+            person: a person object
+
+        Returns:
+            SafeString: the list of filmworks html wrapped
+        """
+        filmworks_html_list = ''
+        for filmwork in person.filmworks.all():
+            filmwork_url = reverse('admin:movies_filmwork_change', args=[filmwork.id])
+            filmworks_html_list = f"""
+                                  {filmworks_html_list}
+                                  <li><a href="{filmwork_url}">{filmwork.title}</a></li>
+                                  """
+
+        filmworks_html_list = f'<ul>{filmworks_html_list}</ul>'
+        return mark_safe(filmworks_html_list)
