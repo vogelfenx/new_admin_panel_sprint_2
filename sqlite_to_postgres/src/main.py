0e7859a2e9b42ef8bec48d51b3e268e5872080c4
Kirill Fedyakov <kirill.fed.pro@gmail.com>
2023-02-02 03:14:46 +0100

    Implement extract data from sqlite function
    - extract data from a table using columns that are set dynamically.
    - check if the requested table exists
    - check if the requested columns exist

--- sqlite_to_postgres/src/main.py
+++ sqlite_to_postgres/src/main.py
@@ -7,7 +7,27 @@ import psycopg2
 from psycopg2.extensions import connection as _connection
 from psycopg2.extras import DictCursor
 
+from sqlite_db_handler import SQLiteExtractor
+
 dotenv.load_dotenv()
+
+
+def load_from_sqlite(sqlite_conn: sqlite3.Connection, pg_conn: _connection):
+    """Основной метод загрузки данных из SQLite в Postgres."""
+    sqlite_extractor = SQLiteExtractor(sqlite_conn)
+    # postgres_saver = PostgresSaver(pg_conn)
+
+    data = sqlite_extractor.extract_data(from_table='genre',
+                                         columns=(
+                                             'id',
+                                             'name',
+                                             'description',
+                                             'created_at',
+                                             'updated_at',
+                                         ))
+    # postgres_saver.save_all_data(data)
+
+
 if __name__ == '__main__':
     dsn_postgres = {
         'dbname': 'movies_database',
---
+++ sqlite_to_postgres/src/sqlite_db_handler.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+
+class SQLiteExtractor:
+    """SQLiteExtractor."""
+
+    def __init__(self, connection: sqlite3.Connection):
+        """_summary_.
+
+        Args:
+            connection (sqlite3.Connection): _description_
+        """
+        self.connection = connection
+        self.connection.row_factory = sqlite3.Row
+        self.cursor = connection.cursor()
+
+    def _check_table_consistency(self, *, table_name):
+        sql_query = """
+        SELECT
+            name
+        FROM
+            sqlite_master
+        WHERE
+            type = 'table'
+            AND name = ?
+            LIMIT 1;
+        """
+
+        self.cursor.execute(sql_query, (table_name, ))
+        is_table_exists = self.cursor.fetchone() is not None
+
+        if not is_table_exists:
+            raise sqlite3.OperationalError(f"table doesn't exist: {table_name}")
+
+    def _check_columns_consistency(self, *, columns, table):
+        sql_query = """
+        SELECT name FROM pragma_table_info(?);
+        """
+        self.cursor.execute(sql_query, (table, ))
+        columns_in_table = self.cursor.fetchall()
+
+        columns_in_table = (column[0] for column in columns_in_table)
+
+        if not all(column in columns_in_table for column in columns):
+            raise sqlite3.OperationalError('Columns do not match the columns in the table.')
+
+    def extract_data(self, *, from_table, columns):
+        cursor = self.cursor
+
+        self._check_table_consistency(table_name=from_table)
+
+        self._check_columns_consistency(columns=columns, table=from_table)
+
+        columns = ','.join(columns)
+        cursor.execute(f'SELECT {columns} FROM {from_table}')
+
+        data = cursor.fetchall()
+        print(dict(data[0]))
