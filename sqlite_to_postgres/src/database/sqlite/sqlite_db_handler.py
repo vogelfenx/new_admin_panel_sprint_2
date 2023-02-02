1fffd8b2e863a3b5936fbd4154c5cfd7eac6b656
Kirill Fedyakov <kirill.fed.pro@gmail.com>
2023-02-03 00:17:09 +0100

    Implement reading data by a given chunk size

--- sqlite_to_postgres/src/database/sqlite/sqlite_db_handler.py
+++ sqlite_to_postgres/src/database/sqlite/sqlite_db_handler.py
@@ -13,16 +13,20 @@ class SQLiteExtractor:
         self.connection = connection
         self.connection.row_factory = sqlite3.Row
         self.cursor = connection.cursor()
+        self.offset = 0
 
-    def extract_data(self, *, from_table, columns):
-        cursor = self.cursor
+    def extract_data(self, *, from_table, columns, chunk_size):
+        chunk_size = int(chunk_size)
 
         self._check_table_consistency(table_name=from_table)
 
         self._check_columns_consistency(columns=columns, table=from_table)
 
+        cursor = self.cursor
+
         columns = ','.join(columns)
-        cursor.execute(f'SELECT {columns} FROM {from_table}')
+        cursor.execute(f'SELECT {columns} FROM {from_table} LIMIT {chunk_size} OFFSET {self.offset}')
+        self.offset += chunk_size
 
         fetched_rows = cursor.fetchall()
 
--- sqlite_to_postgres/src/main.py
+++ sqlite_to_postgres/src/main.py
@@ -14,28 +14,41 @@ from database.table_dataclasses import TableMetadata
 dotenv.load_dotenv()
 
 
-def load_from_sqlite(sqlite_conn: sqlite3.Connection, pg_conn: _connection):
+def load_from_sqlite(sqlite_conn: sqlite3.Connection, pg_conn: _connection, chunk_size):
     """Основной метод загрузки данных из SQLite в Postgres."""
     sqlite_extractor = SQLiteExtractor(sqlite_conn)
     # postgres_saver = PostgresSaver(pg_conn)
 
     tables_meta_sqlite = {
         TableMetadata('person', sqlite.Person.get_fields(), sqlite.Person),
-        TableMetadata('genre', sqlite.Genre.get_fields(), sqlite.Genre),
+        # TableMetadata('genre', sqlite.Genre.get_fields(), sqlite.Genre),
     }
 
     for table_meta in tables_meta_sqlite:
-        table_data = sqlite_extractor.extract_data(
-            from_table=table_meta.table_name, columns=table_meta.columns,
-        )
 
-        dataclass_objects = ()
-        for row_data in table_data:
-            dataclass_object = table_meta.data_class(**row_data)
-            dataclass_objects += (dataclass_object, )
+        is_table_fetch_empty = False
+        while not is_table_fetch_empty:
+            dataclass_objects = ()
+            table_data = sqlite_extractor.extract_data(
+                from_table=table_meta.table_name, columns=table_meta.columns, chunk_size=chunk_size,
+            )
+            if not table_data:
+                is_table_fetch_empty = True
+                break
 
-        print(f'Table {dataclass_objects[0].__class__} has {len(dataclass_objects)} records')
-    # postgres_saver.save_all_data(data)
+            # dataclass_objects = ()
+            for row_data in table_data:
+                dataclass_object = table_meta.data_class(**row_data)
+                dataclass_objects += (dataclass_object, )
+
+            print([i.full_name for i in dataclass_objects])
+            # breakpoint()
+
+            #############
+            # postgres_saver.save_all_data(data)
+            #############
+
+        # print(f'Table {dataclass_objects[0].__class__} has {len(dataclass_objects)} records')
 
 
 if __name__ == '__main__':
@@ -51,4 +64,4 @@ if __name__ == '__main__':
 
     with closing(sqlite3.connect(dsn_sqlite)) as sqlite_conn:
         with closing(psycopg2.connect(**dsn_postgres, cursor_factory=DictCursor)) as pg_conn:
-            load_from_sqlite(sqlite_conn, pg_conn)
+            load_from_sqlite(sqlite_conn, pg_conn, chunk_size=10)
