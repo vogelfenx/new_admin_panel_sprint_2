5b00935f43db4675ff66f1f4e27d458134a3737d
Kirill Fedyakov <kirill.fed.pro@gmail.com>
2023-02-09 22:53:38 +0100

    Increase performance by using prefetch_related
    - overwrite get_queryset method
    - remove duplicates from queryset

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import mark_safe
 from django.utils.translation import gettext_lazy as _
+from django.db.models import Prefetch, QuerySet
+from django.http import HttpRequest
 
 from movies.models import (Filmwork, Genre, GenreFilmwork, Person,
                            PersonFilmwork)
@@ -40,7 +42,22 @@ class PersonAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'get_filmworks')
     search_fields = ('full_name', 'personfilmwork__film_work__title')
 
-    @admin.display(ordering='filmwork__title', description='Кинопроизведения')
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        """Overwrite get_queryset method.
+
+        Args:
+            request (HttpRequest): http request
+
+        Returns:
+            QuerySet: QuerySet of Persons
+        """
+        queryset = super(PersonAdmin, self).get_queryset(request)
+        queryset = queryset.prefetch_related(
+            Prefetch('filmworks', queryset=Filmwork.objects.distinct())
+        )
+        return queryset
+
+    @admin.display(description=_('Filmworks'))
     def get_filmworks(self, person):
         """Return filmworks of the given person.
 
@@ -51,7 +68,9 @@ class PersonAdmin(admin.ModelAdmin):
             SafeString: the list of filmworks html wrapped
         """
         filmworks_html_list = ''
-        for filmwork in person.filmworks.all():
+        person_filmworks = person.filmworks.all()
+
+        for filmwork in person_filmworks:
             filmwork_url = reverse('admin:movies_filmwork_change', args=[filmwork.id])
             filmworks_html_list = f"""
                                   {filmworks_html_list}
