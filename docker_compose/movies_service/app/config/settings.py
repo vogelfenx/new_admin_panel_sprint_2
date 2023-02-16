bace4ada3b322e05e12a82210005a373c15102ad
Kirill Fedyakov <kirill.fed.pro@gmail.com>
2023-02-16 01:21:17 +0100

    Set postgres for the app

--- docker_compose/movies_service/app/config/settings.py
+++ docker_compose/movies_service/app/config/settings.py
@@ -58,9 +58,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),
+        'OPTIONS': {
+            'options': '-c search_path=public,content',
+        },
+    },
 }
 
 
