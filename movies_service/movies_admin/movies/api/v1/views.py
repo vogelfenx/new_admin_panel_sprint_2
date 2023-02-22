d9f1b407190887a33d25ec2c5f446a25f1537986
Kirill Fedyakov <kirill.fed.pro@gmail.com>
2023-02-22 02:21:07 +0100

    Implement pagination in MoviesListApi

--- movies_service/movies_admin/movies/api/v1/views.py
+++ movies_service/movies_admin/movies/api/v1/views.py
@@ -1,6 +1,9 @@
+from typing import Any
+
 from django.contrib.postgres.aggregates import ArrayAgg
+from django.core.paginator import Paginator
 from django.db.models import Q, QuerySet
-from django.http import JsonResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.views.generic.list import BaseListView
 
 from movies.models import Filmwork, PersonFilmwork
@@ -9,6 +12,12 @@ from movies.models import Filmwork, PersonFilmwork
 class MoviesListApi(BaseListView):
     model = Filmwork
     http_method_names = ['get']
+    paginate_by = 10
+
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if not self.request.GET.get('page'):
+            self.paginate_by = None
+        return super().get(request, *args, **kwargs)
 
     def get_queryset(self) -> QuerySet:
         genres = ArrayAgg('genres__name', distinct=True)
@@ -41,11 +50,35 @@ class MoviesListApi(BaseListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         filmworks = object_list if object_list is not None else self.object_list
-        results_count = filmworks.count()
-        context = {
-            'count': results_count,
+        if self.paginate_by:
+            paginator, page, filmworks, is_paginated = self.paginate_queryset(
+                filmworks,
+                self.paginate_by,
+            )
+
+            context = {
+                'count': paginator.count,
+                'total_pages': paginator.num_pages,
+            }
+
+            if page.has_previous():
+                context.update({
+                    'prev': page.previous_page_number(),
+                })
+            if page.has_next():
+                context.update({
+                    'next': page.next_page_number(),
+                })
+
+        else:
+            context = {
+                'count': filmworks.count(),
+            }
+
+        context.update({
             'results': list(filmworks),
-        }
+        })
+
         return context
 
     def render_to_response(self, context, **response_kwargs):
