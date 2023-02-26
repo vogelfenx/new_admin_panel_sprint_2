976dde075d311314326a2f38987f2d402806a439
Kirill Fedyakov <kirill.fed.pro@gmail.com>
2023-02-26 23:02:40 +0100

    Add connection check before makemigrations
    - install netcat in Dockerfile
    - use .env file on startup of the web-app container to setup the environment
    - add wait-loop before database makemigrations in the entrypoint script

--- movies_service/movies_admin/docker-entrypoint.sh
+++ movies_service/movies_admin/docker-entrypoint.sh
@@ -4,6 +4,9 @@ echo "Collect static files"
 python manage.py collectstatic --noinput
 
 echo "Apply database migrations"
+while ! nc -z $DB_HOST $DB_PORT; do
+      sleep 0.1
+done
 python manage.py migrate
 
 echo "Starting uwsgi server"
